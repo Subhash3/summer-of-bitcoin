648748487780fba5ed67f50d8140b4163d892c1e
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-06-13 23:45:04 +0530

    Added main

--- main.py
+++ main.py
@@ -54,7 +54,8 @@ def Main() :
     ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ##
 
     # Analyse an exported block file
-    block_file = "./resources/block_sample.txt"
+    print("\n\n")
+    block_file = "./blocks/block_4094954.0.txt"
     analyse_block_file(block_file, txid_to_transaction_map, txid_to_index_map)
 
 if __name__ == "__main__":
