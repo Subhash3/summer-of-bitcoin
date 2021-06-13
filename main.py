9036be0d848e8db1a53e075acb86d2bc8d31d316
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-06-13 19:44:42 +0530

    Added main file

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-from helpers import parse_mempool_csv, construct_block, export_block
+from helpers import parse_mempool_csv
 from MempoolTransaction import MempoolTransaction
 import typing
+from Block import Block
 
 transactions: typing.List[MempoolTransaction] = list()
-transactions, txid__to_transaction_map = parse_mempool_csv('./resources/mempool.csv')
+transactions, txid_to_transaction_map = parse_mempool_csv('./resources/mempool.csv')
 n = len(transactions)
 # print(n)
 
 # t = transactions[526]
 # print(t)
 # for ptxid in t.parents :
-#     print(txid__to_transaction_map[ptxid])
+#     print(txid_to_transaction_map[ptxid])
 
 weight_limit = 100000
-max_profit, block = construct_block(weight_limit, transactions, n)
-print(block)
+b = Block(weight_limit, transactions, n)
+max_profit, block_array = b.construct()
+print(f"{len(block_array)} transactions are there in the block")
 
 print(f"Max profit: {max_profit}")
+b.export()
+is_valid_block = b.validate(txid_to_transaction_map)
+print(f"is_valid_block: {is_valid_block}")
+
 # for txid in block :
-#     print(txid__to_transaction_map[txid])
-export_block(max_profit, block)
+#     print(txid_to_transaction_map[txid])
+
+# block_file = "./blocks/block_1998524.0.txt"
+# with open(block_file)  as f :
+#     data = f.readlines()
+#     block_array = [line.strip() for line in data]
+
+#     is_valid_block = Block.validate_block_array(block_array, txid_to_transaction_map)
+#     print(is_valid_block)
