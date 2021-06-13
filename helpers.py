a8a0b71b455b3fcef03c209b1e493162bb333193
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-06-13 18:15:10 +0530

    Added numba to optimise knapsack

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 from MempoolTransaction import MempoolTransaction
 import typing
 import time
+from numba import jit
 
 def parse_mempool_csv(mempool_file):
     """Parse the CSV file and return a list of MempoolTransactions."""
@@ -68,12 +69,57 @@ def knapsack_generalized(total_weight, items:typing.List[MempoolTransaction], n)
 
     return max_profit_bkup, used_items
 
-def construct_block(weight_limit, transactions: typing.List[MempoolTransaction], n) :
+@jit(nopython=True)
+def knapsack2(total_weight, weights, profits, item_ids, n):
+    """
+        Solves the knapsack problem.
+        This has been taken from https://www.geeksforgeeks.org/printing-items-01-knapsack/ and modified to suit my needs.
+    """
+    lookup = [[0 for w in range(total_weight + 1)]
+            for i in range(n + 1)]
+
+    for i in range(n + 1):
+        for w in range(total_weight + 1):
+            if i == 0 or w == 0:
+                lookup[i][w] = 0
+            elif weights[i-1] <= w:
+                lookup[i][w] = max(profits[i-1] + lookup[i - 1][w - weights[i-1]], lookup[i - 1][w])
+            else:
+                lookup[i][w] = lookup[i - 1][w]
+
+
+    max_profit = lookup[n][total_weight]
+    # print(max_profit)
+    max_profit_bkup = max_profit
+
+    used_items = list()
+
+    w = total_weight
+    for i in range(n, 0, -1):
+        if max_profit <= 0:
+            break
+
+        if max_profit == lookup[i - 1][w]:
+            continue
+        else:
+            # print(items[i - 1].weight)
+            used_items.append(item_ids[i - 1])
+            max_profit = max_profit - profits[i-1]
+            w = w - weights[i-1]
+
+    return max_profit_bkup, used_items
+
+
+def construct_block(weight_limit, transactions, n) :
     print(f"Weight limit: {weight_limit}")
     print(f"Total transactions: {n}")
 
+    weights = [item.weight for item in transactions]
+    profits = [item.fee for item in transactions]
+    item_ids = [item.txid for item in transactions]
+
     start_time = time.time()
-    max_profit, used_items = knapsack_generalized(weight_limit, transactions, n)
+    max_profit, used_items = knapsack2(weight_limit, weights, profits, item_ids, n)
     end_time = time.time()
     time_taken = round(end_time - start_time, 3)
 
