beaf68e5e545d8a08e7aaf6f54e5ad579bb37107
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-06-13 14:31:37 +0530

    Overrode print method in MempoolTransaction

--- MempoolTransaction.py
+++ MempoolTransaction.py
@@ -2,7 +2,9 @@ class MempoolTransaction():
     def __init__(self, txid, fee, weight, parents):
         self.txid = txid
         self.fee = int(fee)
+        self.weight = int(weight)
+        self.parents = parents.strip().split(';')
         # TODO: add code to parse weight and parents fields
 
     def __str__(self):
-        return f"txtid: {self.txtid}\nfee: {self.fee}\nweight: {self.weight}\nparents: {self.parents}"
+        return f"txid: {self.txid}\nfee: {self.fee}\nweight: {self.weight}\nparents: {self.parents}"
