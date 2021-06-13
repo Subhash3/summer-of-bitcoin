3b81f61ed4c3e69ce4bf094359e31e6b1cfeb326
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-06-13 17:31:11 +0530

    Fixed a minor bug

--- main.py
+++ main.py
@@ -7,22 +7,15 @@ import typing
 transactions: typing.List[MempoolTransaction] = list()
 transactions, txid__to_transaction_map = parse_mempool_csv('./resources/mempool.csv')
 n = len(transactions)
-print(n)
+# print(n)
 
 # t = transactions[526]
 # print(t)
 # for ptxid in t.parents :
 #     print(txid__to_transaction_map[ptxid])
 
-
-
-profits = [ 60, 100, 120 ]
-weights = [ 10, 20, 30 ]
-total_weight = 50
-n = len(profits)
-
-max_profit, used_items = knapsack_generalized(40000, transactions, n)
-print(max_profit)
+max_profit, used_items = knapsack_generalized(4000, transactions, n)
+print(f"Maximized fee: {max_profit}")
 
 for txid in used_items :
     print(txid__to_transaction_map[txid])
