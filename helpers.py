2a4d93023c7e49717affc9138bc5c0e39c0622aa
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-06-13 14:31:49 +0530

    Fixed parsing issue

--- helpers.py
+++ helpers.py
@@ -3,5 +3,8 @@ from MempoolTransaction import MempoolTransaction
 def parse_mempool_csv(mempool_file):
     """Parse the CSV file and return a list of MempoolTransactions."""
     with open(mempool_file) as f:
-        return([MempoolTransaction(*line.strip().split(',')) for line in f.readlines()])
+        data  = f.readlines()
+        data = data[1:] # first line contains header
+
+        return([MempoolTransaction(*line.strip().split(',')) for line in data])
 
