77a5a65d5d5ff077de3913f2f26415e32d2a1d43
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-06-14 16:25:06 +0530

    Combined parent transactions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from helpers import parse_mempool_csv, describe_transactions_list
+from helpers import parse_mempool_csv, describe_transactions_list, knapsack_generalized
 from MempoolTransaction import MempoolTransaction
 import typing
 from Block import Block
@@ -22,7 +22,49 @@ def analyse_block_file(block_file, txid_to_transaction_map, txid_to_index_map) :
         is_valid_block = Block.validate_block_array(block_array, txid_to_transaction_map, txid_to_index_map)
         print(f"is_valid_block: {is_valid_block}")
 
+def construct_and_export(weight_limit, transactions, txid_to_transaction_map, txid_to_index_map) :
+    n = len(transactions)
+    b = Block(weight_limit, transactions, n)
+    max_profit, block_array = b.construct()
+    print(f"{len(block_array)} transactions are there in the block")
 
+    print(f"Max profit: {max_profit}")
+    # b.export()
+    is_valid_block = b.validate(txid_to_transaction_map, txid_to_index_map)
+    print(f"is_valid_block: {is_valid_block}")
+
+
+def combine_parents_util(tx: MempoolTransaction, txid_to_transaction_map, all_parents) :
+    if len(tx.parents) == 0 :
+        # print(tx.txid, tx.fee, tx.weight)
+        return tx.txid, tx.fee, tx.weight
+
+    combined_id = tx.txid
+    combined_fee = tx.fee
+    combined_weight = tx.weight
+    for ptxid in tx.parents :
+        all_parents.add(ptxid)
+        ptx = txid_to_transaction_map[ptxid]
+        new_id, new_fee, new_weight = combine_parents_util(ptx, txid_to_transaction_map, all_parents)
+        combined_id = combined_id + ";" + new_id
+        combined_fee += new_fee
+        combined_weight += new_weight
+    return combined_id, combined_fee, combined_weight
+        
+
+def combine_parents(transactions: typing.List[MempoolTransaction], txid_to_transaction_map) :
+    all_parents = set()
+    combined_parents_transactions:typing.List[MempoolTransaction] = list()
+    for tx in transactions :
+        if len(tx.parents) != 0 :
+            combined_id, combined_fee, combined_weight = combine_parents_util(tx, txid_to_transaction_map, all_parents)
+            combined_tx = MempoolTransaction(combined_id, combined_fee, combined_weight, [])
+            combined_parents_transactions.append(combined_tx)
+        else :
+            if tx.txid not in all_parents:
+                combined_parents_transactions.append(tx)
+    return combined_parents_transactions
+    
 
 
 def Main() :
@@ -36,27 +78,24 @@ def Main() :
     for i in range(total_no_of_transactions) :
         txid_to_index_map[transactions[i].txid] = i
 
+
+    combined_parents_transactions = combine_parents(transactions, txid_to_transaction_map)
+    describe_transactions_list(combined_parents_transactions, "Combined parents transactions")
+
     # Get transactions without any parent transactions
-    transactions_without_parents = get_transactions_without_parents(transactions)
-    describe_transactions_list(transactions_without_parents, "Transactions without parents")
+    # transactions_without_parents = get_transactions_without_parents(transactions)
+    # describe_transactions_list(transactions_without_parents, "Transactions without parents")
 
     ## ## Constructing and exporting a block ## ## #
-    weight_limit = 12345
-    n = len(transactions_without_parents)
-    b = Block(weight_limit, transactions_without_parents, n)
-    max_profit, block_array = b.construct()
-    print(f"{len(block_array)} transactions are there in the block")
-
-    print(f"Max profit: {max_profit}")
-    b.export()
-    is_valid_block = b.validate(txid_to_transaction_map, txid_to_index_map)
-    print(f"is_valid_block: {is_valid_block}")
+    weight_limit = 43210
+    construct_and_export(weight_limit, transactions, txid_to_transaction_map, txid_to_index_map)
+    construct_and_export(weight_limit, combined_parents_transactions, txid_to_transaction_map, txid_to_index_map)
     ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ##
 
-    # Analyse an exported block file
-    print("\n\n")
-    block_file = "./blocks/block_4094954.0.txt"
-    analyse_block_file(block_file, txid_to_transaction_map, txid_to_index_map)
+    # # Analyse an exported block file
+    # print("\n\n")
+    # block_file = "./blocks/block_4094954.0.txt"
+    # analyse_block_file(block_file, txid_to_transaction_map, txid_to_index_map)
 
 if __name__ == "__main__":
     Main()
